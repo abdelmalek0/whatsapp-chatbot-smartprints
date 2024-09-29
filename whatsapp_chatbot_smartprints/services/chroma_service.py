ddc684ad2343e693f8341cb8d60a9cdd62d4e9c8
abdelmalek0 <djamaa.abdelmalek98@gmail.com>
2024-09-29 14:35:32 +0300

    Added JinaEmbeddings

--- whatsapp_chatbot_smartprints/services/chroma_service.py
+++ whatsapp_chatbot_smartprints/services/chroma_service.py
@@ -5,21 +5,35 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import TextLoader
 from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_community.embeddings import JinaEmbeddings
 from langchain_core.documents import Document
+from dotenv import load_dotenv
 
 from utility import normalize_distance_reversed
 
+load_dotenv()
 
 class ChromaService:
     def __init__(self, collection_name, persist_directory, embedding_model_name):
         self.collection_name = collection_name
         self.persist_directory = persist_directory
-        self.embed_model = OllamaEmbeddings(model=embedding_model_name)
+        self.embed_model = JinaEmbeddings(
+            jina_api_key=os.getenv('EMBED_API_KEY'), 
+            name="jina-embeddings-v3",
+            model_config={
+                'protected_namespaces': (),  # Set this to an empty tuple
+                # other configurations
+            }
+        )#jina-embeddings-v3 #OllamaEmbeddings(model=embedding_model_name)
+        
         self.chroma = Chroma(
             collection_name=self.collection_name,
             embedding_function=self.embed_model,
             persist_directory=self.persist_directory,
-            relevance_score_fn=normalize_distance_reversed
+            # relevance_score_fn=normalize_distance_reversed,
+            collection_metadata={
+                "hnsw:space": "cosine"
+            }
         )
         self.text_splitter = RecursiveCharacterTextSplitter(
             separators=["\n\n"], 
@@ -28,7 +42,7 @@ class ChromaService:
         )
 
     def index_files(self, summarizer, batch_size=32):
-        print('Indexing files ...')
+        print('Retrieving files ...')
         documents: List[Document] = []
         for file in os.listdir("./docs/"):
             filepath = os.path.join("./docs/", file)
@@ -41,16 +55,19 @@ class ChromaService:
             texts.extend(doc.page_content.split('$$$$$$$$$$$$$$$$'))
 
         texts = [text for text in texts if text]
-
+        print('Embedding files ...')
+        embeddings = self.embed_model.embed_documents(texts)
+        print('Indexing files ...')
         try:
             self.chroma.add_texts(
                 ids=[f'{idx}' for idx in range(len(texts))],
                 texts= texts,
-                embeddings= self.embed_model.embed_documents(texts)
+                embeddings= embeddings
                 )
         except Exception as e:
             print(f"Error indexing files: {e}")
-        print('Indexing files into the database: 🟩 Finished')
+        else:
+            print('Indexing files into the database: 🟩 Finished')
 
     def retrieve(self, query: str, score_threshold=.8, k=5) -> List[Document]:
         return self.chroma.as_retriever(
